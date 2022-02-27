40a3f8f0cae1e2cb2db3d44ee8ad7423fc07fb31
the-web800 <99961576+the-web800@users.noreply.github.com>
2022-02-27 13:02:12 +0900

    Add files via upload

---
+++ nextf-10.js
@@ -0,0 +1,12 @@
+function nextf() {
+    var gettext = document.getElementById("textinp");
+    var textv = gettext.value;
+    var uncorrect = document.getElementById("notc");
+    var whatcont = document.getElementById("whatc").textContent;
+    if (textv == whatcont) {
+        uncorrect.style.visibility = "hidden";
+        window.location = "./type-6.html";
+    } else {
+        uncorrect.style.visibility = "visible";
+    }
+}
